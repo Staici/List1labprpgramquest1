fcecc4df08b594975706d7da99cc10dc31667e9b
Steicie Aleixo Sousa <51095043+Staici@users.noreply.github.com>
2024-04-08 22:11:04 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@List1labprpgramquest1/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+  int x;
+  printf("O valor inicial é: %d\n valor secundário é : %d \n valor terceário: %d",x=7 + 3*6/2-1, x=2%2+2*2-2/2,x=(3 * 9 * (3 + (9*3/ (3) ) ) ));
+  return 0;
+}
